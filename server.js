8a816b9a90399e94c12f393be56ebf67b02389e7
ItsYashitaa <156766513+ItsYashitaa@users.noreply.github.com>
2025-08-06 17:19:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+// console.log("Hello world");
+
+// const math = require('./math.js');
+// const result = math.add(2,3);
+import {add,subract,multiplication,divide} from  'module-yashita';
+console.log(add(2,3));
+console.log(subract(2,3));
+console.log(multiplication(2,3));
+console.log(divide(2,3));
